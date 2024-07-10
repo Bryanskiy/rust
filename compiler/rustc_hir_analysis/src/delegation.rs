65d73197f01b3f1192f6915a10cc5c532f123126
Bryanskiy <ivakin.kir@gmail.com>
2024-07-10 16:55:40 +0300

    Add builders

--- compiler/rustc_hir_analysis/src/delegation.rs
+++ compiler/rustc_hir_analysis/src/delegation.rs
@@ -4,7 +4,7 @@ use rustc_hir::def::DefKind;
 use rustc_hir::def_id::{DefId, LocalDefId};
 use rustc_middle::ty::fold::{TypeFoldable, TypeFolder, TypeSuperFoldable};
 use rustc_middle::ty::{self, Ty, TyCtxt};
-use rustc_span::ErrorGuaranteed;
+use rustc_span::{ErrorGuaranteed, Span};
 use rustc_type_ir::visit::TypeVisitableExt;
 
 type RemapTable = FxHashMap<u32, u32>;
@@ -75,6 +75,127 @@ fn fn_kind<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId) -> FnKind {
     }
 }
 
+struct GenericsBuilder<'tcx> {
+    tcx: TyCtxt<'tcx>,
+    sig_id: DefId,
+    parent: Option<DefId>,
+    own_params: Vec<ty::GenericParamDef>,
+}
+
+impl<'tcx> GenericsBuilder<'tcx> {
+    fn new(tcx: TyCtxt<'tcx>, sig_id: DefId) -> GenericsBuilder<'tcx> {
+        GenericsBuilder { tcx, sig_id, parent: None, own_params: vec![] }
+    }
+
+    fn with_parent(mut self, parent: DefId) -> Self {
+        self.parent = Some(parent);
+        self
+    }
+
+    fn with_own_generics(mut self, def_id: DefId) -> Self {
+        let generics = self.tcx.generics_of(def_id);
+        self.own_params.append(&mut generics.own_params.clone());
+        self
+    }
+
+    fn with_generics(mut self, def_id: DefId) -> Self {
+        let generics = self.tcx.generics_of(def_id);
+        if let Some(parent_def_id) = generics.parent {
+            self = self.with_own_generics(parent_def_id);
+        }
+        self.with_own_generics(def_id)
+    }
+
+    fn reindex(mut self, offset: u32) -> Self {
+        for (idx, param) in self.own_params.iter_mut().enumerate() {
+            param.index = idx as u32 + offset;
+            // Default type parameters are not inherited: they are not allowed
+            // in fn's.
+            if let ty::GenericParamDefKind::Type { synthetic, .. } = param.kind {
+                param.kind = ty::GenericParamDefKind::Type { has_default: false, synthetic }
+            }
+        }
+        self
+    }
+
+    fn sort(mut self) -> Self {
+        self.own_params.sort_by_key(|key| key.kind.is_ty_or_const());
+        self
+    }
+
+    fn build(self) -> ty::Generics {
+        let param_def_id_to_index =
+            self.own_params.iter().map(|param| (param.def_id, param.index)).collect();
+
+        let (parent_count, has_self) = self
+            .parent
+            .and_then(|parent| {
+                let parent_generics = self.tcx.generics_of(parent);
+                let parent_kind = self.tcx.def_kind(parent);
+                Some((parent_generics.count(), parent_kind == DefKind::Trait))
+            })
+            .unwrap_or((0, false));
+
+        let callee_generics = self.tcx.generics_of(self.sig_id);
+
+        ty::Generics {
+            parent: self.parent,
+            parent_count,
+            own_params: self.own_params,
+            param_def_id_to_index,
+            has_self,
+            has_late_bound_regions: callee_generics.has_late_bound_regions,
+            host_effect_index: callee_generics.host_effect_index,
+        }
+    }
+}
+
+struct PredicatesBuilder<'tcx> {
+    tcx: TyCtxt<'tcx>,
+    args: ty::GenericArgsRef<'tcx>,
+    parent: Option<DefId>,
+    preds: Vec<(ty::Clause<'tcx>, Span)>,
+}
+
+impl<'tcx> PredicatesBuilder<'tcx> {
+    fn new(tcx: TyCtxt<'tcx>, args: ty::GenericArgsRef<'tcx>) -> PredicatesBuilder<'tcx> {
+        PredicatesBuilder { tcx, args, parent: None, preds: vec![] }
+    }
+
+    fn with_parent(mut self, parent: DefId) -> Self {
+        self.parent = Some(parent);
+        self
+    }
+
+    fn with_own_preds<F>(mut self, f: F, def_id: DefId) -> Self
+    where
+        F: Fn(DefId) -> ty::GenericPredicates<'tcx>,
+    {
+        let preds = f(def_id).instantiate_own(self.tcx, self.args);
+        self.preds.extend(preds);
+        self
+    }
+
+    fn with_preds<F>(mut self, f: F, def_id: DefId) -> Self
+    where
+        F: Fn(DefId) -> ty::GenericPredicates<'tcx> + Copy,
+    {
+        let preds = f(def_id);
+        if let Some(parent_def_id) = preds.parent {
+            self = self.with_own_preds(f, parent_def_id);
+        }
+        self.with_own_preds(f, def_id)
+    }
+
+    fn build(self) -> ty::GenericPredicates<'tcx> {
+        ty::GenericPredicates {
+            parent: self.parent,
+            predicates: self.tcx.arena.alloc_from_iter(self.preds),
+            effects_min_tys: ty::List::empty(), // FIXME(fn_delegation): Support effects.
+        }
+    }
+}
+
 fn create_generic_args<'tcx>(
     tcx: TyCtxt<'tcx>,
     def_id: LocalDefId,
@@ -117,7 +238,6 @@ fn create_generic_args<'tcx>(
             let parent_args =
                 tcx.impl_trait_header(parent).unwrap().trait_ref.instantiate_identity().args;
 
-            let callee_generics = tcx.generics_of(sig_id);
             let trait_args = ty::GenericArgs::identity_for_item(tcx, sig_id);
             let method_args = trait_args.iter().skip(callee_generics.parent_count).collect_vec();
 
@@ -155,66 +275,24 @@ pub(crate) fn inherit_generics_for_delegation_item<'tcx>(
     let caller_kind = fn_kind(tcx, def_id.into());
     let callee_kind = fn_kind(tcx, sig_id);
 
-    let param_def_id_to_index = |own_params: &Vec<ty::GenericParamDef>| {
-        own_params.iter().map(|param| (param.def_id, param.index)).collect()
-    };
+    let builder = GenericsBuilder::new(tcx, sig_id);
 
     // FIXME(fn_delegation): early bound generics are only supported for trait
-    // implementations and free functions. Error was reported in `check_constraints`.
+    // implementations and free functions. Error will be reported in `check_constraints`.
     match (caller_kind, callee_kind) {
-        (FnKind::Free, _) => {
-            let mut own_params = vec![];
-
-            let callee_generics = tcx.generics_of(sig_id);
-            if let Some(parent_sig_id) = callee_generics.parent {
-                let parent_sig_generics = tcx.generics_of(parent_sig_id);
-                own_params.append(&mut parent_sig_generics.own_params.clone());
-            }
-            own_params.append(&mut callee_generics.own_params.clone());
-
-            // lifetimes go first
-            own_params.sort_by_key(|key| key.kind.is_ty_or_const());
-
-            for (idx, param) in own_params.iter_mut().enumerate() {
-                param.index = idx as u32;
-                // Default type parameters are not inherited: they are not allowed
-                // in fn's.
-                if let ty::GenericParamDefKind::Type { synthetic, .. } = param.kind {
-                    param.kind = ty::GenericParamDefKind::Type { has_default: false, synthetic }
-                }
-            }
-
-            Some(ty::Generics {
-                parent: None,
-                parent_count: 0,
-                param_def_id_to_index: param_def_id_to_index(&own_params),
-                own_params,
-                has_self: false,
-                has_late_bound_regions: callee_generics.has_late_bound_regions,
-                host_effect_index: None,
-            })
-        }
+        (FnKind::Free, _) => Some(builder.with_generics(sig_id).sort().reindex(0).build()),
         (FnKind::AssocTraitImpl, FnKind::AssocTrait) => {
-            let callee_generics = tcx.generics_of(sig_id);
-
             let parent = tcx.parent(def_id.into());
             let parent_generics = tcx.generics_of(parent);
             let parent_count = parent_generics.count();
 
-            let mut own_params = tcx.generics_of(sig_id).own_params.clone();
-            for (idx, param) in own_params.iter_mut().enumerate() {
-                param.index = (parent_count + idx) as u32;
-            }
-
-            Some(ty::Generics {
-                parent: Some(parent),
-                parent_count,
-                param_def_id_to_index: param_def_id_to_index(&own_params),
-                own_params,
-                has_self: false,
-                has_late_bound_regions: callee_generics.has_late_bound_regions,
-                host_effect_index: None,
-            })
+            Some(
+                builder
+                    .with_parent(parent)
+                    .with_own_generics(sig_id)
+                    .reindex(parent_count as u32)
+                    .build(),
+            )
         }
         // `sig_id` is taken from corresponding trait method
         (FnKind::AssocTraitImpl, _) => unreachable!(),
@@ -227,32 +305,30 @@ pub(crate) fn inherit_predicates_for_delegation_item<'tcx>(
     def_id: LocalDefId,
     sig_id: DefId,
 ) -> Option<ty::GenericPredicates<'tcx>> {
+    let args = create_generic_args(tcx, def_id, sig_id);
+    let builder = PredicatesBuilder::new(tcx, args);
+
+    let caller_kind = fn_kind(tcx, def_id.into());
+    let callee_kind = fn_kind(tcx, sig_id);
     // FIXME(fn_delegation): early bound generics are only supported for trait
     // implementations and free functions. Error was reported in `check_constraints`.
-    let caller_kind = fn_kind(tcx, def_id.into());
-    if caller_kind != FnKind::Free && caller_kind != FnKind::AssocTraitImpl {
-        return None;
+    match (caller_kind, callee_kind) {
+        (FnKind::Free, _) => {
+            Some(builder.with_preds(|sig_id| tcx.predicates_of(sig_id), sig_id).build())
+        }
+        (FnKind::AssocTraitImpl, FnKind::AssocTrait) => {
+            let parent = tcx.parent(def_id.into());
+            Some(
+                builder
+                    .with_parent(parent)
+                    .with_own_preds(|sig_id| tcx.predicates_of(sig_id), sig_id)
+                    .build(),
+            )
+        }
+        // `sig_id` is taken from corresponding trait method
+        (FnKind::AssocTraitImpl, _) => unreachable!(),
+        _ => None,
     }
-
-    let callee_predicates = tcx.predicates_of(sig_id);
-    let args = create_generic_args(tcx, def_id, sig_id);
-
-    let mut preds = vec![];
-    if let Some(parent_id) = callee_predicates.parent {
-        preds.extend(tcx.predicates_of(parent_id).instantiate_own(tcx, args));
-    }
-    preds.extend(callee_predicates.instantiate_own(tcx, args));
-
-    let parent = match fn_kind(tcx, def_id.to_def_id()) {
-        FnKind::Free => None,
-        _ => Some(tcx.parent(def_id.into())),
-    };
-
-    Some(ty::GenericPredicates {
-        parent,
-        predicates: tcx.arena.alloc_from_iter(preds),
-        effects_min_tys: ty::List::empty(),
-    })
 }
 
 fn check_constraints<'tcx>(tcx: TyCtxt<'tcx>, def_id: LocalDefId) -> Result<(), ErrorGuaranteed> {
