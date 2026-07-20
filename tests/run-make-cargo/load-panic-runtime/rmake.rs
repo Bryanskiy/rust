e1560fe73a6fa283000ab0da43a517c48322a4e3
Bryanskiy <ivakin.kir@gmail.com>
2026-07-20 14:20:50 +0300

    add test

---
+++ tests/run-make-cargo/load-panic-runtime/rmake.rs
@@ -0,0 +1,24 @@
+//@ needs-target-std
+
+use run_make_support::{cargo, path, target};
+
+fn main() {
+    let target_dir = path("target");
+
+    cargo()
+        .args(&[
+            "build",
+            "--release",
+            "--manifest-path",
+            "Cargo.toml",
+            "-Zbuild-std=std",
+            "--target",
+            &target(),
+        ])
+        .env("RUSTC_BOOTSTRAP", "1")
+        // Visual Studio 2022 requires that the LIB env var be set so it can
+        // find the Windows SDK.
+        .env("LIB", std::env::var("LIB").unwrap_or_default())
+        .run_fail()
+        .assert_stderr_contains("duplicate lang item in crate `core`: `sized`");
+}
